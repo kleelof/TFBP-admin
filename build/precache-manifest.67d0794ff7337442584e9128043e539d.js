6e2d4e103a13ef40984cf6193c104d647a59f96b
kleelof <kleelof@yahoo.com>
2021-02-07 09:11:45 -0800

    updates;

---
+++ build/precache-manifest.67d0794ff7337442584e9128043e539d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74e090f3a62230093710fd95c230e1bf",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "5d3d1172661d26f548cd",
+    "url": "/dashboard/static/css/main.7f82c877.chunk.css"
+  },
+  {
+    "revision": "fbb568f0ca33e228c40f",
+    "url": "/dashboard/static/js/2.534247d1.chunk.js"
+  },
+  {
+    "revision": "295876b63b5f289380fbbe3289bddeaa",
+    "url": "/dashboard/static/js/2.534247d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d3d1172661d26f548cd",
+    "url": "/dashboard/static/js/main.24238eb6.chunk.js"
+  },
+  {
+    "revision": "cd0bfe217ebe8152a6d9",
+    "url": "/dashboard/static/js/runtime-main.75171549.js"
+  },
+  {
+    "revision": "8ab6523552bd21826c11c886fc769509",
+    "url": "/dashboard/static/media/daisy_logo.8ab65235.png"
+  },
+  {
+    "revision": "9a2c42ba13899b55b1025730ab862db8",
+    "url": "/dashboard/static/media/loader.9a2c42ba.gif"
+  }
+]);
